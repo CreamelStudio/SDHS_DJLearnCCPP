7669f2677d66e10216903633cc057c6e8658b842
Creamel <lhs739300@gmail.com>
2025-03-31 09:56:44 +0900

    Project Add

---
+++ 2025-03-31/WindowsProject1/framework.h
@@ -0,0 +1,20 @@
+﻿// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+// C RunTime Header Files
+#include <stdlib.h> //표준 라이브러리 정의를 가지고 있는 헤더
+#include <malloc.h> //메모리 정의를 가지고 있는 헤더
+#include <memory.h> //메모리 정의를 가지고 있는 헤더
+#include <tchar.h> //문자 정의를 가지고 있는 헤더
+
+//C++ 런타임 헤더
+#include <iostream>
+#include <vector>
+#include <list>
