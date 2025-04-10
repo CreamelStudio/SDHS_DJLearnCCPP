a38703986ea7b2ec1cd20614caaf772f28f4b252
Creamel <lhs739300@gmail.com>
2025-04-10 09:01:57 +0900

    Edit CPP

--- StepByStep/5th_wnd_Project/5th_wnd_Project.cpp
+++ StepByStep/5th_wnd_Project/5th_wnd_Project.cpp
@@ -15,8 +15,6 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
