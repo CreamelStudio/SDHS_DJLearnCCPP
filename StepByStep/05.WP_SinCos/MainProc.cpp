50e2863a39df65906f88a173987cfe61c1352aad
Creamel <lhs739300@gmail.com>
2025-04-24 10:36:56 +0900

    Analog Watch

---
+++ StepByStep/05.WP_SinCos/MainProc.cpp
@@ -0,0 +1,146 @@
+﻿#include "pch.h"
+#include "MainProc.h"
+
+extern HINSTANCE hInst;
+
+
+RECT       grtWndClient;
+SYSTEMTIME gST;
+
+int OnCreate(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    GetClientRect(hWnd, &grtWndClient);
+    SetTimer(hWnd, 0, 1000/33, NULL);
+    return 0;
+}
+
+int OnTimer(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    GetLocalTime(&gST);
+    InvalidateRect(hWnd, NULL, true);
+    
+    return 0;
+}
+
+int OnCommand(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    int wmId = LOWORD(wParam);
+    // 메뉴 선택을 구문 분석합니다:
+    switch (wmId)
+    {
+    case IDM_ABOUT:
+        DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+        break;
+    case IDM_EXIT:
+        DestroyWindow(hWnd);
+        break;
+    default:
+        return DefWindowProc(hWnd, WM_COMMAND, wParam, lParam);
+    }
+    return 0;
+}
+
+#define PI 3.141592f
+#define RAD(x) PI/180.0f*(x)
+
+int OnPaint(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    static int degRun = 0;
+
+    HDC hdc = BeginPaint(hWnd, &ps);
+    // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+    int sysSX = grtWndClient.left + 1;
+    int sysSY = grtWndClient.top + 1;
+    int sysEX = grtWndClient.right - 1;
+    int sysEY = grtWndClient.bottom -1;
+    int sysCW = (sysEX - sysSX) / 2;
+    int sysCH = (sysEY - sysSY) / 2;
+
+    int startX = 500;
+
+    Rectangle(hdc, sysSX, sysSY, sysEX, sysEY);
+    gdiLine(hdc, startX, sysSY, startX, sysEY); //x좌표가 같음 (수직선)
+    gdiLine(hdc, sysSX, sysCH, sysEX, sysCH); //y좌표가 같음(수평선)
+    
+    gdiLine(hdc, startX / 2, sysSY, startX / 2, sysEY);
+    gdiLine(hdc, sysSX, sysCH - 250, sysEX, sysCH - 250);
+    gdiLine(hdc, sysSX, sysCH + 250, sysEX, sysCH + 250);
+
+    int x, y;
+
+    for (int i = 0; i < 360; i++)
+    {
+        x = i * 2.0f;
+        y = -sin((float)RAD(i)) * 250;
+        SetPixel(hdc, x + startX, y + sysCH, RGB(255, 0, 0));
+    }
+    for (int i = 0; i < 360; i++)
+    {
+        x = i * 2.0f;
+        y = -cos((float)RAD(i)) * 250;
+        SetPixel(hdc, x + startX, y + sysCH, RGB(0, 0, 255));
+    }
+
+    for (int i = 0; i < 360; i++)
+    {
+        x = i * 2.0f;
+        y = -tan((float)RAD(i)) * 250;
+        if(abs(y) < 250) SetPixel(hdc, x + startX, y + sysCH, RGB(0, 0, 0));
+    }
+
+    for (int i = 0; i < 360; i++)
+    {
+        x = cos((float)RAD(i)) * 250;
+        y = -sin((float)RAD(i)) * 250;
+        SetPixel(hdc, x + startX / 2, y + sysCH, RGB(0, 0, 0));
+    }
+
+    (degRun>=360) ? degRun = 0 : degRun++;
+    x = cos((float)RAD(degRun)) * 250;
+    y = -sin((float)RAD(degRun)) * 250;
+    gdiStar(hdc, x + startX / 2, y + sysCH);
+    x = degRun * 2.0f;
+    y = -sin((float)RAD(degRun)) * 250;
+    gdiStar(hdc, x + startX, y + sysCH);
+    x = degRun * 2.0f;
+    y = -cos((float)RAD(degRun)) * 250;
+    gdiStar(hdc, x + startX, y + sysCH);
+    x = degRun * 2.0f;
+    y = -tan((float)RAD(degRun)) * 250;
+    if (abs(y) < 250) gdiStar(hdc, x + startX, y + sysCH);
+
+
+    EndPaint(hWnd, &ps);
+    return 0;
+}
+
+int OnDestroy(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    PostQuitMessage(0);
+    return 0;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:     return OnCreate(hWnd, wParam, lParam);
+    case WM_TIMER:      return OnTimer(hWnd, wParam, lParam);
+    case WM_COMMAND:    return OnCommand(hWnd, wParam, lParam);
+    case WM_PAINT:      return OnPaint(hWnd, wParam, lParam);
+    case WM_DESTROY:    return OnDestroy(hWnd, wParam, lParam);
+    default:            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
---
+++ StepByStep/WP_AnalogClock_01/MainProc.cpp
@@ -0,0 +1,112 @@
+﻿#include "pch.h"
+#include "MainProc.h"
+
+extern HINSTANCE hInst;
+void gdiRotLineCW(HDC hdc, float x, float y, float lineLength, float theta);
+SYSTEMTIME gST;
+
+int OnCreate(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    SetTimer(hWnd, 0, 1000, NULL);
+    return 0;
+}
+
+int OnTimer(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    GetLocalTime(&gST);
+
+    TCHAR strTime[128];
+    wsprintf(strTime, L"%02d : %02d : %02d", gST.wHour, gST.wMinute, gST.wSecond);
+    SetWindowTextW(hWnd, strTime);
+    InvalidateRect(hWnd, NULL, true);
+    
+    return 0;
+}
+
+int OnCommand(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    int wmId = LOWORD(wParam);
+    // 메뉴 선택을 구문 분석합니다:
+    switch (wmId)
+    {
+    case IDM_ABOUT:
+        DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+        break;
+    case IDM_EXIT:
+        DestroyWindow(hWnd);
+        break;
+    default:
+        return DefWindowProc(hWnd, WM_COMMAND, wParam, lParam);
+    }
+    return 0;
+}
+
+#define PI 3.141592f
+#define RAD(x) PI/180.0f*(x)
+
+int OnPaint(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hWnd, &ps);
+
+    // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+    int SecNeddleLength = 140;
+    int MinuteNeddleLength = 140;
+    int HourNeddleLength = 70;
+
+    int SecNeddleX = 0; //초바늘 끝점
+    int SecNeddleY = 0; //초바늘 끝점
+    int SecCenterX = 500; //초바늘 시작점
+    int SecCenterY = 300; //초바늘 시작점
+
+    Ellipse(hdc, -260 + SecCenterX, -260 + SecCenterY, 260 + SecCenterX, 260 + SecCenterY);
+    Ellipse(hdc, -240 + SecCenterX, -240 + SecCenterY, 240 + SecCenterX, 240 + SecCenterY);
+    
+
+    float Sectheta = RAD((float)((gST.wSecond - 22.5) * 6.0f));
+    gdiRotLineCW(hdc, 500, 300, 160, Sectheta);
+    float Mintheta = RAD((float)((gST.wMinute - 22.5) * 6.0f));
+    gdiRotLineCW(hdc, 500, 300, 160, Mintheta);
+    float Hourtheta = RAD((float)((gST.wHour - 4.5) * 30.0f));
+    gdiRotLineCW(hdc, 500, 300, 70, Hourtheta);
+
+    EndPaint(hWnd, &ps);
+    return 0;
+}
+
+void gdiRotLineCW(HDC hdc, float x, float y, float lineLength, float theta) {
+    float tempNeddleX = cos(theta) * lineLength - sin(theta) * lineLength;
+    float tempNeddleY = cos(theta) * lineLength + sin(theta) * lineLength;
+    gdiLine(hdc, x, y, tempNeddleX + x, tempNeddleY + y);
+}
+
+int OnDestroy(HWND hWnd, WPARAM wParam, LPARAM lParam)
+{
+    PostQuitMessage(0);
+    return 0;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:     return OnCreate(hWnd, wParam, lParam);
+    case WM_TIMER:      return OnTimer(hWnd, wParam, lParam);
+    case WM_COMMAND:    return OnCommand(hWnd, wParam, lParam);
+    case WM_PAINT:      return OnPaint(hWnd, wParam, lParam);
+    case WM_DESTROY:    return OnDestroy(hWnd, wParam, lParam);
+    default:            return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
