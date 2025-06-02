4cb6d31de02e2d3e2fa37d47225f080b9c843546
Creamel <lhs739300@gmail.com>
2025-06-02 09:44:44 +0900

    Game Loop Add

---
+++ StepB/WP_GameLoop_01/GameManager.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "GameManager.h"
+
+extern int nFPS;
+extern int nCount;
+extern HDC gHDC;
+
+void Awake()
+{
+
+}
+
+void Start()
+{
+
+}
+
+void Update()
+{
+	nCount++;
+
+	wchar_t strTemp[128] = L"";
+	wsprintf(strTemp, L"FPS = %06d, frame = %06d", nFPS, nCount);
+	TextOut(gHDC, 0, 0, strTemp, wcslen(strTemp));
+}
+
+void Release()
+{
+
+}
